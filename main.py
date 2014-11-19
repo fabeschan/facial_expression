324e40ef079d25b19d187322d62f016df913060d
JiayiOellers <jiayiwang.wang@mail.utoronto.ca>
2014-11-19 18:37:38 -0500

    Tried to evaluate knn. Have broadcast error ln 35

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ Created on Wed Nov 19 16:12:02 2014
 
 @author: fabian
 @author: jiayi
+
 """
 
 import scipy.io
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import cross_validation
 import run_knn
@@ -16,9 +18,23 @@ tr_images = imagesDic["tr_images"]
 tr_identity = imagesDic["tr_identity"]
 tr_labels = imagesDic["tr_labels"]
 
+nTrainExample = 2462
+
+train_img = tr_images[:,:,:nTrainExample]
+valid_img = tr_images[:,:,nTrainExample:]
+
+train_labels = tr_labels[:nTrainExample]
+valid_labels = tr_labels[nTrainExample:]
+
+"""
 plt.figure(1)
 plt.clf()
-
 plt.imshow(tr_images[:,:,2], cmap=plt.cm.gray)
-#plt.draw()
 plt.show()
+"""
+
+labels = run_knn.run_knn(3, train_img, train_labels, valid_img)
+
+false_count = np.flatnonzero(valid_labels - labels).size
+rate = float(valid_labels.size - false_count)/valid_labels.size
+print(rate)

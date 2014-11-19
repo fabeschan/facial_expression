40db3535fe4d119cbd88c37039d7022f780d4a07
JiayiOellers <jiayiwang.wang@mail.utoronto.ca>
2014-11-19 17:48:34 -0500

    Load and visualize test data

--- main.py
+++ main.py
@@ -5,5 +5,16 @@ Created on Wed Nov 19 16:12:02 2014
 @author: jiayi
 """
 import scipy.io
+import matplotlib.pyplot as plt
+
 imagesDic = scipy.io.loadmat(file_name="labeled_images.mat")
-print "something"
+tr_images = imagesDic["tr_images"]
+tr_identity = imagesDic["tr_identity"]
+tr_labels = imagesDic["tr_labels"]
+
+plt.figure(1)
+plt.clf()
+
+#plt.subplot(1, v.shape[1], i+1)
+plt.imshow(tr_images[:,:,0].reshape(32, 32).T, cmap=plt.cm.gray)
+plt.draw()

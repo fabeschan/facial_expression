4fd62239126342a6ab22b2f3a25435068c6f61f4
JiayiOellers <jiayiwang.wang@mail.utoronto.ca>
2014-11-19 17:24:40 -0500

    Hello World!

---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 19 16:12:02 2014
+
+@author: jiayi
+"""
+import scipy.io
+imagesDic = scipy.io.loadmat(file_name="labeled_images.mat")
+print "something"

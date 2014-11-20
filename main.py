47f239deb805506c8559419eae1e11db5c811590
Fabian Chan <fabeschan@gmail.com>
2014-11-19 19:55:07 -0500

    wip

--- main.py
+++ main.py
@@ -18,13 +18,16 @@ tr_images = imagesDic["tr_images"]
 tr_identity = imagesDic["tr_identity"]
 tr_labels = imagesDic["tr_labels"]
 
-nTrainExample = 2462
+nTrainExample = 2162
 
 train_img = tr_images[:,:,:nTrainExample]
 valid_img = tr_images[:,:,nTrainExample:]
 
 train_labels = tr_labels[:nTrainExample]
 valid_labels = tr_labels[nTrainExample:]
+
+train_img = np.array([ train_img[:,:,i].reshape(-1) for i in xrange(train_img.shape[2]) ])
+valid_img = np.array([ valid_img[:,:,i].reshape(-1) for i in xrange(valid_img.shape[2]) ])
 
 """
 plt.figure(1)
@@ -33,7 +36,17 @@ plt.imshow(tr_images[:,:,2], cmap=plt.cm.gray)
 plt.show()
 """
 
-labels = run_knn.run_knn(3, train_img, train_labels, valid_img)
+#a = np.array([[2,3],[1,2]])
+#b = np.array([[4,3],[3,2]])
+#c = np.array([[2,4],[4,1]])
+#
+#train_img = np.array([a,b,c,a,b,c])
+#train_labels = np.array([2,3,4,2,3,4])
+#valid_img = np.array([c,a,c,b,b,c])
+#train_img = np.array([ train_img[:,:,i].reshape(-1) for i in xrange(train_img.shape[2]) ])
+#valid_img = np.array([ valid_img[:,:,i].reshape(-1) for i in xrange(valid_img.shape[2]) ])
+
+labels = run_knn.run_knn(1, train_img.T, train_labels.T, valid_img.T)
 
 false_count = np.flatnonzero(valid_labels - labels).size
 rate = float(valid_labels.size - false_count)/valid_labels.size

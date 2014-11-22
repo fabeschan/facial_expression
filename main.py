eedd5d0df8e2dd46e2dc2d26abb2a6dd48d51b37
JiayiOellers <jiayiwang.wang@mail.utoronto.ca>
2014-11-21 20:51:01 -0500

    Normalized image. Added function to output labels

--- main.py
+++ main.py
@@ -12,23 +12,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import cross_validation
 from sklearn import datasets, neighbors, linear_model, svm, naive_bayes, tree
+from sklearn import preprocessing
 import run_knn, skLearnStuff
 
 imagesDic = scipy.io.loadmat(file_name="labeled_images.mat")
-tr_images = imagesDic["tr_images"]
-tr_identity = imagesDic["tr_identity"]
+tr_images = imagesDic["tr_images"].astype(float)
+tr_identity = imagesDic["tr_identity"].astype(float)
 tr_labels = imagesDic["tr_labels"]
+
+imagesDic = scipy.io.loadmat(file_name="public_test_images.mat")
+test_images = imagesDic["public_test_images"].astype(float)
+
+tr_images = np.array([tr_images[:,:,i].reshape(-1) for i in xrange(tr_images.shape[2])])
+tr_images = preprocessing.scale(tr_images,1)
+
 
 nTrainExample = 2162
 
-train_img = tr_images[:,:,:nTrainExample]
-valid_img = tr_images[:,:,nTrainExample:]
+"""
+train_img = tr_images[:nTrainExample,:]
+valid_img = tr_images[nTrainExample:,:]
 
 train_labels = tr_labels[:nTrainExample]
 valid_labels = tr_labels[nTrainExample:]
-
-train_img = np.array([ train_img[:,:,i].reshape(-1) for i in xrange(train_img.shape[2]) ])
-valid_img = np.array([ valid_img[:,:,i].reshape(-1) for i in xrange(valid_img.shape[2]) ])
+"""
 
 """
 plt.figure(1)
@@ -50,11 +57,11 @@ plt.show()
 #labels = run_knn.run_knn(5, train_img, train_labels, train_img)
 
 classifiers = [
-    linear_model.RidgeClassifierCV(),
+    #linear_model.RidgeClassifierCV(),
     #neighbors.NearestNeighbors(n_neighbors=2, algorithm='ball_tree'),
-    neighbors.KNeighborsClassifier(),
-    naive_bayes.GaussianNB(),
-    tree.DecisionTreeClassifier(criterion="entropy"),
+    neighbors.KNeighborsClassifier(p=2),
+    #naive_bayes.GaussianNB(),
+    #tree.DecisionTreeClassifier(criterion="entropy"),
 ]
 
 pred_ensemble = []
@@ -78,3 +85,15 @@ valid_labels = train_labels[0.85 * train_labels.size:]
 false_count = np.flatnonzero(valid_labels.reshape(-1) - pred_voted.reshape(-1)).size
 rate = float(valid_labels.size - false_count)/valid_labels.size
 print(rate)
+
+
+def write_to_file(predictions):
+    with open("csv.csv", 'w') as f:
+        f.write("id,prediction\n")
+        for i in range(len(predictions)):
+            s = "{},{}\n".format(i+1, predictions[i])            
+            f.write(s)
+
+
+a = ['2','3','4','1','2','3']
+write_to_file(pred_voted)
